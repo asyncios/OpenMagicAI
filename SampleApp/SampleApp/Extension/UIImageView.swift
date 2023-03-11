f05672e0b9b2639b660c4b170732f594ab74829a
Franco Cadillo <jfcadillo951@gmail.com>
2023-03-10 22:37:24 -0500

    add sample app

---
+++ SampleApp/SampleApp/Extension/UIImageView.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView.swift
+//  SampleApp
+//
+//  Created by admin on 9/03/23.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SampleApp/SampleApp/ViewController/Cell/UserChatTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserChatTableViewCell.swift
+//  SampleApp
+//
+//  Created by admin on 9/03/23.
+//
+
+import UIKit
+
+final class UserChatTableViewCell: BaseChatTableViewCell {
+
+    static let identifier = "UserChatTableViewCell"
+ 
+}
