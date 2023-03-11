edf31b7ddae3e4bd9be0ad5774176ad07fdc7ad8
Franco Cadillo <jfcadillo951@gmail.com>
2023-03-10 23:45:43 -0500

    add doc for endpoint

--- Sources/OpenMagicAI/OpenMagicAI.swift
+++ Sources/OpenMagicAI/OpenMagicAI.swift
@@ -2,24 +2,28 @@ import Foundation
 
 public final class OpenMagicAI {
 
+    /// List and describe the various models available in the API
+    public private(set) lazy var models: ModelsApi = {
+        .init(apiKey: apiKey)
+    }()
 
+    /// Given a prompt, the model will return one or more predicted completions, and can also return the probabilities of alternative tokens at each position.
     public private(set) lazy var completions: CompletionsApi = {
         .init(apiKey: apiKey)
     }()
 
+    /// Given a chat conversation, the model will return a chat completion response.
     public private(set) lazy var chat: ChatApi = {
         .init(apiKey: apiKey)
     }()
 
-    public private(set) lazy var images: ImagesApi = {
-        .init(apiKey: apiKey)
-    }()
-
+    /// Given a prompt and an instruction, the model will return an edited version of the prompt.
     public private(set) lazy var edits: EditsApi = {
         .init(apiKey: apiKey)
     }()
 
-    public private(set) lazy var models: ModelsApi = {
+    /// Given a prompt and/or an input image, the model will generate a new image.
+    public private(set) lazy var images: ImagesApi = {
         .init(apiKey: apiKey)
     }()
 
